b6a6c71c767230bfc302eda5833f81097c556c78
Ismael <ismaelbd042@gmail.com>
2023-05-09 12:53:28 +0200

    añadimos parte de las vistas

--- ProyectoIntegrador2/src/vista/IVentana.java
+++ ProyectoIntegrador2/src/vista/IVentana.java
@@ -1,5 +1,12 @@
 package vista;
 
+import controlador.Controlador;
+
 public interface IVentana {
 
+	public void inicializarComponentes();
+
+	public void hacerVisible();
+
+	public void setControlador(Controlador c);
 }
