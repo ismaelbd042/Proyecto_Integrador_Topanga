c24f2a835eaaad6448d601ac3cdffbaef926c588
Lucca <luccamanfredotti@gmail.com>
2023-05-16 14:09:24 +0200

    Añadimos datos a los modelos

--- ProyectoIntegrador2/src/modelo/Alumnos.java
+++ ProyectoIntegrador2/src/modelo/Alumnos.java
@@ -1,5 +1,51 @@
 package modelo;
 
 public class Alumnos {
+	
+	private int id_alumno;
+	private String nombre_alumno;
+	private String apellido_alumno;
+	private String num_expediente;
+	
+	public Alumnos(int id_alumno, String nombre_alumno, String apellido_alumno, String num_expediente) {
+		this.id_alumno = id_alumno;
+		this.nombre_alumno = nombre_alumno;
+		this.apellido_alumno = apellido_alumno;
+		this.num_expediente = num_expediente;
+	}
+
+	public int getId_alumno() {
+		return id_alumno;
+	}
+
+	public void setId_alumno(int id_alumno) {
+		this.id_alumno = id_alumno;
+	}
+
+	public String getNombre_alumno() {
+		return nombre_alumno;
+	}
+
+	public void setNombre_alumno(String nombre_alumno) {
+		this.nombre_alumno = nombre_alumno;
+	}
+
+	public String getApellido_alumno() {
+		return apellido_alumno;
+	}
+
+	public void setApellido_alumno(String apellido_alumno) {
+		this.apellido_alumno = apellido_alumno;
+	}
+
+	public String getNum_expediente() {
+		return num_expediente;
+	}
+
+	public void setNum_expediente(String num_expediente) {
+		this.num_expediente = num_expediente;
+	}
+	
+	
 
 }
--- ProyectoIntegrador2/src/modelo/ProyectosIntegradores.java
+++ ProyectoIntegrador2/src/modelo/ProyectosIntegradores.java
@@ -1,5 +1,102 @@
 package modelo;
 
 public class ProyectosIntegradores {
+	
+	private String nombre_proyecto;
+	private String URL;
+	private int componentes;
+	private int año;
+	private String curso;
+	private String grupo;
+	private int nota;
+	private Áreas cod_area;
+	private int[][] alumnoRealizaProyecto;
+	
+	public ProyectosIntegradores(String nombre_proyecto, String URL, int componentes, int año, String curso,
+			String grupo, int nota, Áreas cod_area, int[][] alumnoRealizaProyecto) {
+		this.nombre_proyecto = nombre_proyecto;
+		this.URL = URL;
+		this.componentes = componentes;
+		this.año = año;
+		this.curso = curso;
+		this.grupo = grupo;
+		this.nota = nota;
+		this.cod_area = cod_area;
+		this.alumnoRealizaProyecto = alumnoRealizaProyecto;
+	}
+
+	public String getNombre_proyecto() {
+		return nombre_proyecto;
+	}
+
+	public void setNombre_proyecto(String nombre_proyecto) {
+		this.nombre_proyecto = nombre_proyecto;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
+	public int getComponentes() {
+		return componentes;
+	}
+
+	public void setComponentes(int componentes) {
+		this.componentes = componentes;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	public Áreas getCod_area() {
+		return cod_area;
+	}
+
+	public void setCod_area(Áreas cod_area) {
+		this.cod_area = cod_area;
+	}
+
+	public int[][] getAlumnoRealizaProyecto() {
+		return alumnoRealizaProyecto;
+	}
+
+	public void setAlumnoRealizaProyecto(int[][] alumnoRealizaProyecto) {
+		this.alumnoRealizaProyecto = alumnoRealizaProyecto;
+	}
+	
+	
 
 }
