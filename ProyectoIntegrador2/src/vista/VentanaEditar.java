b5fb070e1b948be2c2237ce3c5cf4d2075225a53
Lucca <luccamanfredotti@gmail.com>
2023-05-16 12:57:12 +0200

    Errores

--- ProyectoIntegrador2/src/vista/VentanaEditar.java
+++ ProyectoIntegrador2/src/vista/VentanaEditar.java
@@ -15,7 +15,7 @@ public class VentanaEditar extends JFrame implements IVentana {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
-	
+
 	public VentanaEditar() {
 		super("Editar proyecto");
 		inicializarComponentes();
@@ -23,38 +23,41 @@ public class VentanaEditar extends JFrame implements IVentana {
 
 	@Override
 	public void inicializarComponentes() {
-getContentPane().setLayout(null);
-		
+		getContentPane().setLayout(null);
+
 		JLabel lblNewLabel = new JLabel("Nombre:");
 		lblNewLabel.setBounds(26, 43, 45, 13);
 		getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Colaboradores:");
 		lblNewLabel_1.setBounds(26, 94, 84, 13);
 		getContentPane().add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Última modificación:");
 		lblNewLabel_2.setBounds(26, 148, 110, 13);
 		getContentPane().add(lblNewLabel_2);
-		
+
 		textField = new JTextField();
 		textField.setBounds(81, 40, 96, 19);
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(106, 91, 96, 19);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setBounds(130, 145, 96, 19);
 		getContentPane().add(textField_2);
 		textField_2.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("Actualizar datos");
 		btnNewButton.setBounds(52, 191, 151, 21);
-		getContentPane().add(btnNewButton);		
+		getContentPane().add(btnNewButton);
+
+		setSize(463, 206);
+		setLocationRelativeTo(null);
 	}
 
 	@Override
@@ -65,26 +68,25 @@ getContentPane().setLayout(null);
 	@Override
 	public void setControlador(ControladorPrincipal_Ver c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setControlador(ControladorModificar_Editar c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setControlador(ControladorPrincipal_Subir c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setControlador(ControladorPrincipal_Modificar c) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 }
