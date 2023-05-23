e5983b328995751960d6b0727bcf6a7ada1594a5
Alvaro <alvaroserrano0109@gmail.com>
2023-05-23 17:41:46 +0200

    Comentamos algunas clases

--- ProyectoIntegrador2/src/controlador/ListenerJlistAlumno.java
+++ ProyectoIntegrador2/src/controlador/ListenerJlistAlumno.java
@@ -6,18 +6,30 @@ import javax.swing.JList;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+/**
+ * 
+ * @author Ismael Bodas, Álvaro Serrano y Lucca Manfredotti
+ *
+ */
 public class ListenerJlistAlumno implements ListSelectionListener {
 
 	static String nombreApellido;
 
+	/**
+	 * Metodo de valueChanged
+	 */
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
+		//Creamos JList llamado list
 		JList<String> list = (JList<String>) e.getSource();
+		//Nueva lista de Strings llamada selected value
 		List<String> selectedValues = list.getSelectedValuesList();
 
+		//Creamos un if para comprobar si el selected value esta vacio
 		if (!selectedValues.isEmpty()) {
 			nombreApellido = selectedValues.get(0);
 
+			//Creamos otro if para mostrar si esta seleccion en curso o seleccion definitiva
 			if (e.getValueIsAdjusting()) {
 				System.out.println("Seleccion en curso: " + ((JList) e.getSource()).getSelectedValuesList());
 			} else {
