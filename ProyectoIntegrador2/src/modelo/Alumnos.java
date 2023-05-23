21200bcb0014cd41a093746d785e6f6c7e580bb4
Alvaro <alvaroserrano0109@gmail.com>
2023-05-23 11:59:29 +0200

    Comentamos las clases del paquete modelo

--- ProyectoIntegrador2/src/modelo/Alumnos.java
+++ ProyectoIntegrador2/src/modelo/Alumnos.java
@@ -1,12 +1,24 @@
 package modelo;
 
+/**
+ * 
+ * @author Ismael Bodas, Álvaro Serrano, Lucca Manfredotti
+ *
+ */
 public class Alumnos {
 	
+	//Creamos las variables de la clase alumno
 	private int id_alumno;
 	private String nombre_alumno;
 	private String apellido_alumno;
 	private String num_expediente;
-	
+	/**
+	 * Constructor de la Clase Alumnos con los diferentes parametros
+	 * @param id_alumno id del alumno
+	 * @param nombre_alumno nombre del alumno
+	 * @param apellido_alumno apellido del alumno
+	 * @param num_expediente numero de expediente del alumno
+	 */
 	public Alumnos(int id_alumno, String nombre_alumno, String apellido_alumno, String num_expediente) {
 		this.id_alumno = id_alumno;
 		this.nombre_alumno = nombre_alumno;
@@ -14,34 +26,66 @@ public class Alumnos {
 		this.num_expediente = num_expediente;
 	}
 
+	/**
+	 * Getter de la variable id_alumno
+	 * @return id_alumno id del alumno
+	 */
 	public int getId_alumno() {
 		return id_alumno;
 	}
 
+	/**
+	 * Setter de la variable id_alumno
+	 * @param id_alumno id del alumno
+	 */
 	public void setId_alumno(int id_alumno) {
 		this.id_alumno = id_alumno;
 	}
 
+	/**
+	 * Getter de la variable nombre_alumno
+	 * @return nombre_alumno nombre del alumno
+	 */
 	public String getNombre_alumno() {
 		return nombre_alumno;
 	}
 
+	/**
+	 * Setter de la variable nombre_alumno
+	 * @param nombre_alumno nombre del alumno
+	 */
 	public void setNombre_alumno(String nombre_alumno) {
 		this.nombre_alumno = nombre_alumno;
 	}
 
+	/**
+	 * Getter de la variable apellido_alumno
+	 * @return apellido_alumno apellido del alumno
+	 */
 	public String getApellido_alumno() {
 		return apellido_alumno;
 	}
 
+	/**
+	 * Setter de la variable apellido_alumno
+	 * @param apellido_alumno apellido del alumno
+	 */
 	public void setApellido_alumno(String apellido_alumno) {
 		this.apellido_alumno = apellido_alumno;
 	}
 
+	/**
+	 * Getter de la variable num_expediente
+	 * @return num_expediente numero de expediente del alumno
+	 */
 	public String getNum_expediente() {
 		return num_expediente;
 	}
 
+	/**
+	 * Setter de la variable num_expediente
+	 * @param num_expediente numero de expediente del alumno
+	 */
 	public void setNum_expediente(String num_expediente) {
 		this.num_expediente = num_expediente;
 	}
--- ProyectoIntegrador2/src/modelo/Áreas.java
+++ ProyectoIntegrador2/src/modelo/Áreas.java
@@ -1,27 +1,54 @@
 package modelo;
 
+/**
+ * 
+ * @author Ismael Bodas, Álvaro Serrano y Lucca Manfredotti
+ *
+ */
 public class Áreas {
 	
+	//Creamos las variables de la clase Áreas
 	private String nombre_area;
 	private String descripcion;
 	
+	/**
+	 * Creamos el constructor de la clase Áreas
+	 * @param nombre_area nombre del área
+	 * @param descripcion descripción del área
+	 */
 	public Áreas(String nombre_area, String descripcion) {
 		this.nombre_area = nombre_area;
 		this.descripcion = descripcion;
 	}
 
+	/**
+	 * Getter de la variable nombre_area
+	 * @return nombre_area nombre del area
+	 */
 	public String getNombre_area() {
 		return nombre_area;
 	}
 
+	/**
+	 * Setter de la variable nombre_area
+	 * @param nombre_area nombre del area
+	 */
 	public void setNombre_area(String nombre_area) {
 		this.nombre_area = nombre_area;
 	}
 
+	/**
+	 * Getter de la variable descripcion
+	 * @return descripcion descripcion del área
+	 */
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+	/**
+	 * Setter de la variable descripcion
+	 * @param descripcion descripcion del área
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
